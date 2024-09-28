8f4e30d87b784a3b617167d988d3855adcd6930d
df1oe <df1oe@live.de>
2024-09-28 22:53:16 +0200

    added fastfotobackup and SortFilesToFolders

---
+++ SortFilesToFolders.ps1
@@ -0,0 +1,24 @@
+﻿[string] $SourceDirectoryPath = 'Z:\fotos\Urlaub-2024\Leica\Roland'
+[string] $TargetDirectoryPath = 'D:\Transport\2024'
+
+[System.Collections.ArrayList] $filesToMove = Get-ChildItem -Path $SourceDirectoryPath -File -Recurse
+
+$filesToMove | ForEach-Object {
+    [System.IO.FileInfo] $file = $_
+
+    [DateTime] $fileDate = $file.LastWriteTimeUtc
+    Write-Output $fileDate
+    [string] $dateDirectoryName = $fileDate.ToString('yyyy-MM-dd')
+    [string] $dateDirectoryPath = Join-Path -Path $TargetDirectoryPath -ChildPath $dateDirectoryName
+    Write-Output $dateDirectoryPath   
+     if (!(Test-Path -Path $dateDirectoryPath -PathType Container))
+    {
+        Write-Verbose "Creating directory '$dateDirectoryPath'."
+        New-Item -Path $dateDirectoryPath -ItemType Directory -Force > $null
+    }
+
+    [string] $filePath = $file.FullName
+   Write-Output $file.FullName
+   Write-Information "Moving file '$filePath' into directory '$dateDirectoryPath'."
+   Copy-Item -Path $filePath -Destination $dateDirectoryPath
+}
---
+++ fastfotobackup.ps1
@@ -0,0 +1,11 @@
+﻿##   param([string] $directory)
+$dirs = Get-ChildItem -path "D:\Transport" -Attributes Directory | Select-Object Name
+#$date =  Get-Date -Format ddMMyyyy
+$dest = "\\apollon\public1\fotos\roland\Transport"
+#$args = "/MIR /FFT /Z /XA:H /MT:8 /W:5 /R:5"
+foreach ($dir in $dirs)
+{
+$source = "D:\Transport\" + $dir.Name
+$destdir = $dest +"\"+ $dir.Name 
+Start-Process robocopy -args "$source $destdir /MIR /FFT /Z /XA:H /MT:8 /W:5 /R:5"
+}
